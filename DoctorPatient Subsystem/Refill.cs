c97e919b75123cfb169ecf989b7bef98c5e9ce84
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2019-12-04 16:14:20 -0500

    Add files via upload
    Modified Refill class.

--- DoctorPatient Subsystem/Refill.cs
+++ DoctorPatient Subsystem/Refill.cs
@@ -50,9 +50,36 @@ namespace DoctorPatient_Subsystem
 
         }
 
-        public void getRefillData()
+        public void getRefillData(int id)
         {
-
+            //prepare an SQL query to retrieve the patient 
+            DataTable myTable = new DataTable();
+            string connStr = "server=csdatabase.eku.edu;user=stu_csc340;database=csc340_db;port=3306;password=Colonels18;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                string sql = "JOIN statement?? for id of patient's refill -- retrieve refill info?" + id; //placeholder for the real table name
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                //cmd.Parameters.AddWithValue("@myDate", dateString);
+                MySqlDataAdapter myAdapter = new MySqlDataAdapter(cmd);
+                myAdapter.Fill(myTable);
+                Console.WriteLine("Table is ready.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            //convert the retrieved data to events and save them to the list
+            foreach (DataRow row in myTable.Rows)
+            {
+                timePeriod = row["timePeriod"].ToString();
+                timesFilled = Int32.Parse(row["timesFilled"].ToString());
+                medication = row["medication"].ToString();
+                noticeID = Int32.Parse(row["noticeID"].ToString());
+            }
         }
 
     }
