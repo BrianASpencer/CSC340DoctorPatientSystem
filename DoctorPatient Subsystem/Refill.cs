540ff77c5c633794d5ff2007b77fe3a243e1c3f0
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2019-12-04 12:23:56 -0500

    Add files via upload
    Added Refill class.

---
+++ DoctorPatient Subsystem/Refill.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DoctorPatient_Subsystem
+{
+    class Refill
+    {
+        string timePeriod;
+        int timesFilled;
+        string medication;
+        int noticeID;
+
+        public string getTimePeriod()
+        {
+            return this.timePeriod;
+        }
+
+        public int getTimesFilled()
+        {
+            return this.timesFilled;
+        }
+
+        public string getMedication()
+        {
+            return this.medication;
+        }
+
+        public int getNoticeID()
+        {
+            return this.noticeID;
+        }
+
+        public void createRefill()
+        {
+
+        }
+
+        public void retrieveRefillData()
+        {
+
+        }
+
+        public void updateRefill()
+        {
+
+        }
+
+        public void getRefillData()
+        {
+
+        }
+
+    }
+}
