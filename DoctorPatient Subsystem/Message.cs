24d098e972c1dfe105e3b99c09f9065d5355f596
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2019-12-04 12:18:38 -0500

    Add files via upload
    Added Message and Prescription classes.

---
+++ DoctorPatient Subsystem/Message.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace DoctorPatient_Subsystem
+{
+    class Message
+    {
+        int noticeID;
+        string typeOfNotice;
+        string date;
+        int doctorID;
+        int prescriptionID;
+        int refillID;
+        string message;
+        int appointment;
+
+        Message(int notID, string type, string date, int docID, int preID, int reID, string msg, int app)
+        {
+            this.noticeID = notID;
+            this.typeOfNotice = type;
+            this.date = date;
+            this.doctorID = docID;
+            this.prescriptionID = docID;
+            this.refillID = reID;
+            this.message = msg;
+            this.appointment = app;
+        }
+
+        public int getNoticeID()
+        {
+            return this.noticeID;
+        }
+
+        public string getTypeOfNotice()
+        {
+            return this.typeOfNotice;
+        }
+
+        public string getDate()
+        {
+            return this.date;
+        }
+
+        public int getDoctorID()
+        {
+            return this.doctorID;
+        }
+
+        public int getPrescriptionID()
+        {
+            return this.prescriptionID;
+        }
+
+        public int getRefillID()
+        {
+            return this.refillID;
+        }
+
+        public string getMessage()
+        {
+            return this.message;
+        }
+
+        public int getAppointment()
+        {
+            return this.appointment;
+        }
+
+        public void createMessage()
+        {
+            //SQL INSERT Statement goes here
+        }
+
+    }
+}
---
+++ DoctorPatient Subsystem/Prescription.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DoctorPatient_Subsystem
+{
+    class Prescription
+    {
+        int prescriptionID;
+        string status;
+        string receivedTime;
+        string pickUpTime;
+        string medication;
+        int noticeID;
+
+        public int getPrescriptionID()
+        {
+            return this.prescriptionID;
+        }
+
+        public string getStatus()
+        {
+            return this.status;
+        }
+
+        public string getReceivedTime()
+        {
+            return this.receivedTime;
+        }
+
+        public string getPickUpTime()
+        {
+            return this.pickUpTime;
+        }
+
+        public string getMedication()
+        {
+            return this.medication;
+        }
+
+        public int getNoticeID()
+        {
+            return this.noticeID;
+        }
+
+        public void createPrescription()
+        {
+            //insert statement for DB?
+        }
+
+        public void prescriptionData()
+        {
+
+        }
+
+        public void updatePrescription()
+        {
+            //maybe delete table entry in DB and insert new one?
+        }
+
+    }
+}
