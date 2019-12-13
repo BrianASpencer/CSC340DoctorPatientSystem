4e6fee770a2dcc014da613f27840da134ed1ce4d
KodiD <40518298+KodiD@users.noreply.github.com>
2019-12-12 21:18:16 -0500

    made prescription hooked up to db an fixed issue with message.

--- DoctorPatient Subsystem/Message.cs
+++ DoctorPatient Subsystem/Message.cs
@@ -26,16 +26,18 @@ namespace DoctorPatient_Subsystem
 
         }
 
-        public Message(int notID, string type, string date, int docID, int preID, int reID, string msg, int app)
+        public Message(int notID, string type, string date, int docID, int patID, int preID, int reID, string msg, int app, bool grant)
         {
             this.noticeID = notID;
             this.typeOfNotice = type;
             this.date = date;
             this.doctorID = docID;
-            this.prescriptionID = docID;
+            this.patientID = patID;
+            this.prescriptionID = preID;
             this.refillID = reID;
             this.message = msg;
             this.appointment = app;
+            this.isGranted = grant;
         }
 
         public Message(string type, int docID, int patID, string msg)
@@ -124,7 +126,55 @@ namespace DoctorPatient_Subsystem
 
         public ArrayList retrieveMessagesList(string whereVar, int key)
         {
-            return null;
+            ArrayList messageList = new ArrayList();
+
+            DataTable myTable = new DataTable();
+            string connStr = "server=csdatabase.eku.edu;user=stu_csc340;database=csc340_db;port=3306;password=Colonels18;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            //prepare an SQL query to retrieve notice i from patient from patients
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                string sql = "SELECT * FROM kodibrian_message WHERE @var = @key ;"; 
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@var", whereVar);
+                cmd.Parameters.AddWithValue("@key", key);
+                MySqlDataAdapter myAdapter = new MySqlDataAdapter(cmd);
+                myAdapter.Fill(myTable);
+                Console.WriteLine("Table is ready.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            //convert the retrieved data to events and save them to the list
+            foreach (DataRow row in myTable.Rows)
+            {
+                int notId = Int32.Parse(row["notice_id"].ToString());
+                string type = row["typeofNotice"].ToString();
+                string dateT = row["mdate"].ToString();
+                int docId = Int32.Parse(row["doctor_id"].ToString());
+                int patId = Int32.Parse(row["patient_id"].ToString());
+                int preId = Int32.Parse(row["prescription_id"].ToString());
+                int reId = Int32.Parse(row["refill_id"].ToString());
+                string mes = row["message"].ToString();
+                int appId = Int32.Parse(row["appointment_id"].ToString());
+                string status = row["iGranted"].ToString();
+                bool grant = status.Equals("True");
+
+                messageList.Add(new Message(notId, type, dateT, docId, patId, preId, reId, mes, appId, grant));
+
+            }
+
+
+            if (messageList.Count == 0)
+            {
+                return null;
+            }
+            return messageList;
         }
 
     }
--- DoctorPatient Subsystem/Prescription.cs
+++ DoctorPatient Subsystem/Prescription.cs
@@ -11,11 +11,29 @@ namespace DoctorPatient_Subsystem
     class Prescription
     {
         int prescriptionID;
+        int doctorID;
+        int patientID;
         string status;
         string receivedTime;
         string pickUpTime;
         string medication;
         int noticeID;
+
+        public Prescription()
+        {
+
+        }
+
+        public Prescription(int docId, int patId, string stat, string rTime, string pTime, string med)
+        {
+            doctorID=docId;
+            patientID=patId;
+            status=stat;
+            receivedTime=rTime;
+            pickUpTime=pTime;
+            medication=med;
+            createPrescription();
+        }
 
         public int getPrescriptionID()
         {
@@ -47,19 +65,85 @@ namespace DoctorPatient_Subsystem
             return this.noticeID;
         }
 
+        public int getDoctorID()
+        {
+            return this.doctorID;
+        }
+
+        public int getPatientID()
+        {
+            return this.patientID;
+        }
+
         public void createPrescription()
         {
-            //insert statement for DB?
-        }
+            // prepare an SQL query to retrieve
+            DataTable myTable = new DataTable();
+            string connStr = "server=csdatabase.eku.edu;user=stu_csc340;database=csc340_db;port=3306;password=Colonels18;";
+            MySqlConnection conn = new MySqlConnection(connStr);
 
-        public void prescriptionData()
-        {
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                string sql = "INSERT INTO kodibrian_prescriptions (pstatus, receivedTime, medication)VALUES (@status , @rTime , @med );";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@status", status);
+                cmd.Parameters.AddWithValue("@rTime", receivedTime);
+                cmd.Parameters.AddWithValue("@med", medication);               
 
-        }
+                //excute and get preID
+                prescriptionID = Convert.ToInt32(cmd.ExecuteScalar());
+                Console.WriteLine("Done.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
 
-        public void updatePrescription()
-        {
-            //maybe delete table entry in DB and insert new one?
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                string sql = "INSERT INTO kodibrian_message (typeofNotice, mdate, doctor_id, patient_id, prescription_id)VALUES (@type , @date , @docId, @patId, @preId );";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@type", "Pre");
+                cmd.Parameters.AddWithValue("@date", receivedTime);
+                cmd.Parameters.AddWithValue("@preId", prescriptionID);
+                cmd.Parameters.AddWithValue("@patId", doctorID);
+                cmd.Parameters.AddWithValue("@docId", patientID);
+
+                //excute and get NoteID
+                noticeID = Convert.ToInt32(cmd.ExecuteScalar());
+                Console.WriteLine("Done.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+            //update appointment in table
+
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                string sql = "UPDATE kodibrian_prescriptions SET notice_id = @noteId WHERE prescription_id = @preId";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@noteId", noticeID);
+                cmd.Parameters.AddWithValue("@preId", prescriptionID);
+
+                //excute
+                cmd.ExecuteScalar();
+                Console.WriteLine("Done.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
         }
 
     }
